887ff7c506941adc1e4e6709b1b3700c8efa6501
Alan Thomas <alanmt555@gmail.com>
2026-06-22 01:22:34 -0300

    descomentar rutas de productos en AppRoutes

--- crud-tienda-back/src/routes/routes.ts
+++ crud-tienda-back/src/routes/routes.ts
@@ -1,15 +1,15 @@
-import { Router } from "express";
-import productoRouter from "./producto-router/producto.router.js";
-import authRouter from "./auth-router/auth.routes.js";
-export class AppRoutes {
+    import { Router } from "express";
+    import productoRouter from "./producto-router/producto.router.js";
+    import authRouter from "./auth-router/auth.routes.js";
+    export class AppRoutes {
 
-    static get routes(): Router {
-        const router = Router();
+        static get routes(): Router {
+            const router = Router();
 
-        //router.use("/api/productos", productoRouter);
-        router.use("/api/auth", authRouter);
+            router.use("/api/productos", productoRouter);
+            router.use("/api/auth", authRouter);
 
-        return router
+            return router
+        }
+
     }
-
-}
