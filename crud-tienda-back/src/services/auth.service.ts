a7aea08fe6ddfe4a67f4584c74caf35010be921f
facundo-Benitez <faezbenitez@gmail.com>
2026-06-25 23:16:41 +0100

    refactor: mejorar validación de datos y manejo de errores en registrar usuarios

--- crud-tienda-back/src/services/auth.service.ts
+++ crud-tienda-back/src/services/auth.service.ts
@@ -2,30 +2,35 @@
 import type { UsuarioRepository } from '../repository/usuario.repository.js';
 import type { Usuario } from '../models/usuario.model.js';
 import { encrypt } from '../utils/bcrypt.js';
+import { ValidationError } from '../utils/validation-error.js';
 
 export class AuthService {
   constructor(private usuarioRepository: UsuarioRepository) {}
 
   async registrarUsuario(usuario: Usuario) {
-    const { email, contrasena, nombre, apellido, direccion} = usuario;
+    const nombre = usuario.nombre?.trim();
+    const apellido = usuario.apellido?.trim();
+    const email = usuario.email?.trim();
+    const direccion = usuario.direccion?.trim();
+    const contrasena = usuario.contrasena;
 
-    if (!nombre || typeof nombre !== "string") throw new Error('El nombre es obligatorio');
-    if (!apellido || typeof apellido !== "string") throw new Error('El apellido es obligatorio');
-    if (!direccion || typeof direccion !== "string") throw new Error('La dirección es obligatoria');
+    if (!nombre || typeof nombre !== "string") throw new ValidationError('El nombre es obligatorio');
+    if (!apellido || typeof apellido !== "string") throw new ValidationError('El apellido es obligatorio');
+    if (!direccion || typeof direccion !== "string") throw new ValidationError('La dirección es obligatoria');
     
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!email || !emailRegex.test(email)) {
-      throw new Error('Formato de email inválido');
+      throw new ValidationError('Formato de email inválido');
     }
 
     const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
     if (!contrasena || !passwordRegex.test(contrasena)) {
-      throw new Error('La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número.');
+      throw new ValidationError('La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula y un número.');
     }
 
     const usuarioExistente = await this.usuarioRepository.findUsuarioByEmail(email);
     if (usuarioExistente) {
-      throw new Error('Email inválido, ya está en uso');
+      throw new ValidationError('Email inválido, ya está en uso',409);
     }
 
     const hashPassword = await encrypt(contrasena);
