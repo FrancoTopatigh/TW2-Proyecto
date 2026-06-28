39e19e3983a101f611ec8cb3ca09cf2fff6a4184
Nach098 <ignaciomosquera1998@gmail.com>
2026-06-27 22:33:48 -0300

    Revert "Suma y resta de articulos en pagina de pedido"
    This reverts commit daab6bc6888894b981d03d071fbb777072992361.

--- crud-tienda/src/app/api/services/productos/productos.service.spec.ts
+++ crud-tienda/src/app/api/services/productos/productos.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http'; // 👈 IMPORTANTE: Agregá esto también
+import { provideHttpClientTesting } from '@angular/common/http/testing'; // 👈 Y esto
 
-
+// 🚀 CAMBIO CLAVE: Cambiar 'Productos' por 'ProductosService'
 import { ProductosService } from './productos.service';
 
 describe('ProductosService', () => {
@@ -10,6 +10,7 @@ describe('ProductosService', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
+      // 💡 REGLA DE ORO: Como tu servicio usa HttpClient, hay que darle los mocks para que el test no explote
       providers: [
         ProductosService,
         provideHttpClient(),
